ebce84f54b1e6079e1f600b3db0745c0b30b220a
Los Alexander <sasha.los.0148@gmail.com>
2025-07-19 16:18:29 +0500

    fix: disable async local storage for paraglide

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
+			disableAsyncLocalStorage: true,
 			strategy: ['url', 'cookie', 'baseLocale']
 		})
 	]
